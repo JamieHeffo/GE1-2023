40baa02b724636861b2a84ff4069cef75f460f92
JamieHeffo <125657344+JamieHeffernan@users.noreply.github.com>
2023-09-29 16:11:18 +0100

    Tank Game With Boxes

--- themetaverse/tank1.gd
+++ themetaverse/tank1.gd
@@ -1,4 +1,12 @@
 extends CharacterBody3D
 
+@export var move_speed = 5
+@export var rot_speed = 5
+
 func _process(delta):	
-		translate(Vector3.FORWARD * delta)
+		var move = Input.get_axis("move_forward", "move_backwards")
+		var turn = Input.get_axis("move_left", "move_right")
+		print(move)
+		
+		translate(Vector3.FORWARD * move * delta * move_speed)
+		rotate(Vector3.UP, turn * rot_speed * delta)
